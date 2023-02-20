cfa36ef157e0b41b7c604a6b15c67f23a760b82c
Chris Collinsworth <cmcollin41@gmail.com>
2023-02-20 01:08:28 -0700

    swell

--- components/navigation/site-footer.tsx
+++ components/navigation/site-footer.tsx
@@ -7,23 +7,13 @@ export function SiteFooter() {
       <div className="flex  flex-col items-center justify-between gap-4 border-t border-t-slate-200 py-10 dark:border-t-slate-700 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-slate-600 dark:text-slate-400 md:text-left">
-            Inspired by{" "}
             <a
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4 mr-3"
-            >
-              shadcn
-            </a>
-
-            Extended for e-commerce by  <a
               href={siteConfig.links.twitter}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              storefront.dev
+              Swell Commerce
             </a>
           </p>
         </div>
