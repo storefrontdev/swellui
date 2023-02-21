cf10803e168e3289b5b98eb18a09142734d4ef74
Chris Collinsworth <cmcollin41@gmail.com>
2023-02-21 09:58:43 -0700

    upgrades

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,14 +1,4 @@
 self.__FONT_LOADER_MANIFEST={
   "pages": {},
-  "app": {
-    "/Users/chrisc/Desktop/www/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Poppins\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-sans\",\"display\":\"swap\",\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\"]}],\"variableName\":\"fontSans\"}": [
-      "static/media/7a78f1ce0329757f.p.woff2",
-      "static/media/d36a2a2bb416f59e.p.woff2",
-      "static/media/c04551857776278f.p.woff2",
-      "static/media/916d3686010a8de2.p.woff2",
-      "static/media/7777133e901cd5ed.p.woff2",
-      "static/media/d869208648ca5469.p.woff2",
-      "static/media/9a881e2ac07d406b.p.woff2"
-    ]
-  }
+  "app": {}
 }
